e82a1688aa269a57abefc1d7ac8dd4fbe3d0805b
william Chorey <williamchorey@williams-MacBook-Pro.local>
2015-10-16 17:46:22 -0400

    Friday afternoon upload

--- grok_site/js/script.js
+++ grok_site/js/script.js
@@ -2,4 +2,30 @@
 
 (function( window, undefined ){
 
+console.log( $ );
+var dayButtons = $(".schedule_buttons a")
+
+var divName = $(".schedule div")
+
+dayButtons.on("click", function( e ){
+
+	var className = $( this ).data( "page");
+
+	divName.hide();
+	$( className ).fadeIn( 1000 );
+
+	$( className ).show();
+
+	e.preventDefault();
+});
+
+divName.hide();
+
+$( ".d1" ).fadeIn( 1000 );
+
+
+
+
+
+
 })( window, undefined );
