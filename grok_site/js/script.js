e0c066b9c6d7865bb7b95a2f76ba635c1acb8aea
billychorey <billychorey@gmail.com>
2015-11-19 12:13:53 -0500

    Thursday update 11/19

--- grok_site/js/script.js
+++ grok_site/js/script.js
@@ -7,9 +7,16 @@ var dayButtons = $(".schedule_buttons a")
 
 var divName = $(".schedule div")
 
+
+
+
 dayButtons.on("click", function( e ){
 
 	var className = $( this ).data( "page");
+
+
+	
+
 
 	divName.hide();
 	$( className ).fadeIn( 1000 );
@@ -22,6 +29,39 @@ dayButtons.on("click", function( e ){
 divName.hide();
 
 $( ".d1" ).fadeIn( 1000 )
+
+
+// test	
+
+
+
+$( function(){
+
+		var buttons = $( ".schedule_buttons a" );
+		var dayselect      = $( ".btn" );
+		// var pages          = $( ".page" );
+		
+		dayselect.on( "click", function( e ){
+
+			var atag      = $( this );
+			// var classname = atag.data( "page" );
+
+			//changes the tab colors from gray to white when clicked
+	        dayselect.addClass( "active" );
+	        atag.parent().removeClass( "active" );
+
+	        //hide the pages
+			// pages.hide();
+			// //show the page related to the button clicked
+			// $( classname ).show();
+
+			//prevents the page from reloading
+	        e.preventDefault();
+	    });
+
+	    // $( ".current_projects a" ).trigger( "click" );
+	} );
+
 
 
 
